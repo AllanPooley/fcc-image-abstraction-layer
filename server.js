4040ca9f51787a3654414075d17cea555dff422d
Allan Pooley <allanpooley@outlook.com>
2017-04-26 04:32:43 +0000

    Added stubs for functionality to be implemented

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.get('/', function(req, res) {
 
 
 
-app.get('/imagesearch/*', function(req, res) {
+app.get('/imagesearch/:target*', function(req, res) {
+  
+  // Get search term
+  // Get ?offset = query string value.
+  
+  
   
 });
 
@@ -28,4 +33,22 @@ app.get('/history', function(req, res) {
 app.listen(PORT, function (){
     console.log('Listening on port ' + PORT);
 });
+
+
+
+// Connects to a search engine API and retrieves images that match the given term.
+function searchForImages(term, offset){
   
+}
+
+// Saves a search term along with the current DateTime to a database.
+function saveSearch(term) {
+  
+}
+
+// Retrieves saved search history from the database, returning the most recently
+// saved documents.
+function getRecentSearch(){
+  
+}
+
